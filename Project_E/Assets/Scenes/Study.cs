e9ac5b9c633dc799f698191835af28daa242cb3d
NamWon Kim <dress5hirt361@gmail.com>
2025-11-26 16:49:00 +0900

    [ADD] 1일차 실습
    1일차 실습 저장

---
+++ Project_E/Assets/Scenes/Study.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+        Debug.Log("Hello, World!");
+        Debug.Log("Hello, World!");
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
