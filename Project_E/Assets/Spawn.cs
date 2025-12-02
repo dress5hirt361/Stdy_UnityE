c07365e977234de026dc6a0607612a7d7684c0fb
NamWon Kim <dress5hirt361@gmail.com>
2025-12-02 17:04:05 +0900

    [ADD] 5일차 실습 내역 추가
    5일차 실습 내역 추가

---
+++ Project_E/Assets/Spawn.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject targetPrefab;
+    public Transform spawnPosition;
+
+    public void SpawnTarget()
+    {
+       Instantiate(targetPrefab, transform.position, Quaternion.identity);
+    }
+
+}
